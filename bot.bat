0cf2b84e6bb1a22a69fc5a2ee51009b09a2f1302
Opedepodepes <71410992+Opedepodepes-Olugbemi@users.noreply.github.com>
2022-01-23 09:44:40 +0100

    Bot code
    the bot receives input from user and prints it back to the screen

---
+++ bot.bat
@@ -0,0 +1,70 @@
+*First code*
+
+@echo off
+
+Title B-bot && color 0a 
+
+:Start
+
+Cls
+
+echo this is a bot that accepts data and prints it out
+
+Pause >nul
+
+set /p name= Input your name:
+
+if ‰name‰ ==%random% goto pname
+
+:pname
+
+echo Hello %name% 
+
+Pause >nul
+
+echo nice to meet you
+
+set /p age= Input your age:
+
+if %age% ==%random% goto page
+
+:page
+
+echo wow you're %age% years old?!.
+
+Pause >nul
+
+echo I'm just %time% old
+
+Pause >nul
+
+echo awoken %date% by you
+
+Pause >nul
+
+Goto print
+
+:print
+
+Echo so these are your information
+
+Pause >nul
+
+echo name: %name%
+
+Pause >nul
+
+echo age: %age%
+
+Echo thanks for using me :)
+
+echo like and follow me on 
+
+Pause >nul
+
+echo twitter: 
+
+echo facebook:
+
+echo and github 
+
